ec41ef9f6cc91823e72f2db6d5f37dd55d625456
arletino <titkov@nxt.ru>
2024-08-29 15:48:45 +0300

    alpha ready

--- ignore/final/selenium_t/page_object/locators.py
+++
@@ -1,7 +0,0 @@
-from conftest import data_locators
-    
-class TestSearchLocators:
-    ...
-
-for name, value in data_locators().items():
-    setattr(TestSearchLocators, name, value)
--- ignore/final/test_api/test_soap/test_yaspeller.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from .check_text import check_text
-
-
-@pytest.mark.soap
-def test_step(set_words, set_urlsoap):
-    good_word, wrong_word = set_words
-    assert good_word in check_text(wrong_word, wsdl=set_urlsoap)
